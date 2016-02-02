51157f382d7f2e0feb115310cfc65f0fd4e7972c
ForeverGlory <foreverglory@qq.com>
2016-02-02 13:49:22 +0800

    监听事件类型 Signed-off-by:ForeverGlory <foreverglory@qq.com>

---
+++ WechatEvents.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Glory\WechatBundle;
+
+/**
+ * Description of WechatEvents
+ *
+ * @author ForeverGlory
+ */
+final class WechatEvents
+{
+
+    const MESSAGE = 'wechat.message';
+
+}
