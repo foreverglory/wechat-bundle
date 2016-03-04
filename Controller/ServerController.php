26e20e9a2502114b18adbe67dc934d4ef7ad9409
ForeverGlory <foreverglory@qq.com>
2016-03-04 22:52:30 +0800

    消息回复

---
+++ Controller/ServerController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Glory\WechatBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Glory\WechatBundle\Entity\Message;
+use Glory\WechatBundle\Events;
+use Glory\WechatBundle\Event\WechatEvent;
+
+/**
+ * Description of CommController
+ *
+ * @author ForeverGlory
+ */
+class ServerController extends Controller
+{
+
+    public function messageAction(Request $request, $id)
+    {
+        $app = $this->get('wechat.app.' . $id);
+        $dispatcher = $this->get('event_dispatcher');
+        $app->server->setMessageHandler(function($message) use ($app, $dispatcher) {
+            $event = new WechatEvent($app, $message);
+            $dispatcher->dispatch(Events::SERVER, $event);
+            return $event->getResponse();
+        });
+        return $app->server->serve();
+    }
+
+}
