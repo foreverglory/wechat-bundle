95d0914df5bdefdabb3f673cd6046c6f65439640
ForeverGlory <foreverglory@qq.com>
2016-04-09 21:14:58 +0800

    更改 SERVER 服务，更改为 wechat.server_request, wechat.server_response

---
+++ GloryWechatEvents.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+ * (c) ForeverGlory <http://foreverglory.me/>
+ * 
+ * For the full copyright and license information, please view the LICENSE
+ */
+
+namespace Glory\Bundle\WechatBundle;
+
+/**
+ * Description of WechatEvents
+ *
+ * @author ForeverGlory <foreverglory@qq.com>
+ */
+final class GloryWechatEvents
+{
+
+    const SERVER_REQUEST = 'wechat.server_request';
+    const SERVER_RESPONSE = 'wechat.server_response';
+
+}
