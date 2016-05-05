ef16e7d62a0118e5d5469f52a437eacd9be17166
ForeverGlory <foreverglory@qq.com>
2016-05-05 22:04:12 +0800

    将使用 oauth-bundle

--- Wechat/Application.php
+++ Wechat/Application.php
@@ -16,9 +16,6 @@ class Application extends App
     public function __construct(ContainerInterface $container, $config)
     {
         $router = $container->get('router');
-        if (empty($config['oauth']['callback'])) {
-            $config['oauth']['callback'] = $router->generate('glory_wechat_oauth_callback', array(), true);
-        }
         parent::__construct($config);
     }
 
