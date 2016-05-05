2dee0e8c25f9f3a57d8e2a9e336e0f8854a20d79
ForeverGlory <foreverglory@qq.com>
2016-05-05 18:57:26 +0800

    扫码支付

--- Controller/PaymentController.php
+++ Controller/PaymentController.php
@@ -1,11 +1,20 @@
 <?php
 
+/*
+ * (c) ForeverGlory <http://foreverglory.me/>
+ * 
+ * For the full copyright and license information, please view the LICENSE
+ */
+
 namespace Glory\Bundle\WechatBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
-use Glory\Bundle\WechatBundle\WechatEvents;
-use Glory\Bundle\WechatBundle\EventDispatcher\Event;
+use Symfony\Component\HttpFoundation\Response;
+use EasyWeChat\Payment\Merchant;
+use EasyWeChat\Payment\Payment;
+use EasyWeChat\Payment\Order;
+use Endroid\QrCode\QrCode;
 
 /**
  * Description of PayController
@@ -15,29 +24,50 @@ use Glory\Bundle\WechatBundle\EventDispatcher\Event;
 class PaymentController extends Controller
 {
 
-    public function payAction(Request $request, $id)
+    /**
+     * 生成支付二维码
+     * @return Response
+     */
+    public function qrcodeAction(Request $request, $id)
     {
-        
+        $payManager = $this->get('glory_pay.pay_manager');
+        $order = $payManager->getOrder($id);
+        $payment = $this->getPayment();
+        $result = $payment->prepare(new Order([
+            'body' => $order->getBody(),
+            'detail' => $order->getDetail(),
+            'out_trade_no' => $id,
+            'total_fee' => floatval($order->getAmount()) * 100,
+            'trade_type' => 'NATIVE', //扫码支付
+        ]));
+        if ($result->return_code == 'SUCCESS' && $result->result_code == 'SUCCESS') {
+            $url = $result['code_url'];
+        } else {
+            
+        }
+        $qrCode = new QrCode();
+        $qrCode->setText($url)
+                ->setSize(240)
+                ->setPadding(20);
+        $content = $qrCode->get('png');
+        return new Response($content, 200, array('Content-Type' => 'image/png'));
     }
 
-    public function notifyAction(Request $request, $id)
+    protected function getPayment()
     {
-        $app = $this->get('wechat.app.' . $id);
-        $dispatcher = $this->get('event_dispatcher');
-        $response = $app->payment->handleNotify(function($notify, $successful) use ($app, $dispatcher) {
-            //查找本地订单
-//            $order = findOrder($notify->transaction_id);
-//            if (!$order) {
-//                return 'Order not exist'; //告诉微信，我已经处理完了，订单没找到，别再通知我了
-//            }
-            if ($successful) {
-                //支付成功
-            } else {
-                
-            }
-            return true;
-        });
-        return $response;
+        $merchant = new Merchant([
+            'app_id' => $this->container->getParameter('wechat_id'),
+            'merchant_id' => $this->container->getParameter('wechat_partner'),
+            'key' => $this->container->getParameter('wechat_partnerkey'),
+            'notify_url' => $this->getNotifyUrl(),
+        ]);
+        $payment = new Payment($merchant);
+        return $payment;
+    }
+
+    protected function getNotifyUrl()
+    {
+        return $this->generateUrl('glory_pay_notify', ['service' => 'wechat'], true);
     }
 
 }
