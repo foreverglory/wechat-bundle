8aa225cd5f3a39d392aea978f40f436430c973a5
ForeverGlory <foreverglory@qq.com>
2016-11-24 00:31:25 +0800

    使用单一微信服务，如需要管理多微信，请复制 glory_wechat.app 服务

--- DependencyInjection/GloryWechatExtension.php
+++ DependencyInjection/GloryWechatExtension.php
@@ -6,7 +6,6 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\Loader;
-use Symfony\Component\DependencyInjection\DefinitionDecorator;
 
 /**
  * This is the class that loads and manages your bundle configuration
@@ -27,14 +26,7 @@ class GloryWechatExtension extends Extension
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
         $loader->load('services.yml');
 
-//        $container->getDefinition('glory_wechat.app')->addArgument($config['app']);
-//
-//        foreach ($config['apps'] as $key => $val) {
-//            $server = new DefinitionDecorator('glory_wechat.app');
-//            $options = array_merge($val, array('name' => $key));
-//            $server->addArgument($options);
-//            $container->setDefinition(sprintf('glory_wechat.app.%s', $key), $server);
-//        }
+        $container->getDefinition('glory_wechat.app')->addArgument($config['app']);
     }
 
 }
